4755b458a59a45f14771d389dfef3c1cb8c139b8
unknown <a.afif@tgmena.com>
2025-11-09 13:12:25 +0300

    aa

--- src/i18n/resources/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as en} from '@/i18n/resources/en';
-export {default as ar} from '@/i18n/resources/ar';
--- src/utils/month.ts
+++
@@ -1,4 +0,0 @@
-export const calculateMonths = (days: number): number => {
-  const averageDaysInMonth = 30.44;
-  return Math.round(days / averageDaysInMonth);
-};
